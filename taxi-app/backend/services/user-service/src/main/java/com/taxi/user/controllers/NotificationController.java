b98315b58e8fee693910e873822a23db215b021f
Abdusamed Beqiri <beqiriabdusamed@gmail.com>
2025-04-09 01:31:39 +0200

    Fixes

--- taxi-app/backend/services/user-service/src/main/java/com/taxi/user/controllers/NotificationController.java
+++ taxi-app/backend/services/user-service/src/main/java/com/taxi/user/controllers/NotificationController.java
@@ -1,11 +1,32 @@
 package com.taxi.user.controllers;
 
+import com.taxi.user.entities.NotificationToken;
+import com.taxi.user.services.NotificationTokenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/notification")
 public class NotificationController {
+    private final NotificationTokenService notificationTokenService;
+
+    @GetMapping()
+    public List<String> getAllTokens(){
+        return notificationTokenService.getAllTokens();
+    }
+
+    @PostMapping()
+    public Optional<NotificationToken> createToken(NotificationToken notif){
+        return notificationTokenService.createNotification(notif);
+    }
+
+    @GetMapping("/{id}")
+    public List<String> getTokensByUserId(@PathVariable Long id){
+        return notificationTokenService.getTokenByUserId(id);
+    }
+
 }
